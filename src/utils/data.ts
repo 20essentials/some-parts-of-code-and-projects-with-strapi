da1e59616be08baf615495db76a9ead445ff71bd
DominicDev <s0l0estudi4r@gmail.com>
2026-02-24 18:21:38 +0100

    v0.0.1

---
+++ src/utils/data.ts
@@ -0,0 +1,122 @@
+export const arrayCards = [
+  {
+    title: 'qs.stringify',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/frontend/src/lib/strapitUtils.ts',
+    localImage: '/assets/projects/qs-stringify.avif',
+    id: '01'
+  },
+  {
+    title: 'Signup Form + Strapi Errors',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/frontend/src/components/signup-form.tsx',
+    localImage: '/assets/projects/signup-form.avif',
+    id: '02'
+  },
+  {
+    title: 'useActionState + Login Form',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/frontend/src/components/login-form.tsx',
+    localImage: '/assets/projects/login-form.avif',
+    id: '03'
+  },
+  {
+    title: 'Email Simple Plugin',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/config/plugins.ts',
+    localImage: '/assets/projects/email-plugin.avif',
+    id: '04'
+  },
+  {
+    title: 'Draft Mode Config',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/config/admin.ts',
+    localImage: '/assets/projects/draft-mode.avif',
+    id: '05'
+  },
+  {
+    title: 'rateLimit',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/config/plugins.ts',
+    localImage: '/assets/projects/rateLimit.avif',
+    id: '06'
+  },
+  {
+    title: 'Github Oauth + Route Handlers',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/frontend/src/app/api/connect/github/route.ts',
+    localImage: '/assets/projects/github-oauth.avif',
+    id: '07'
+  },
+  {
+    title: 'Generating an OpenAPI specification ',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/specification.json',
+    localImage: '/assets/projects/specifications-openapi.avif',
+    id: '08'
+  },
+  {
+    title: 'Swagger UI compatible Page ',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/public/swagger.html',
+    localImage: '/assets/projects/swagger-ui.avif',
+    id: '09'
+  },
+  {
+    title: 'Custom Router Example',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/src/api/article/routes/article.ts',
+    localImage: '/assets/projects/custom-router.avif',
+    id: '10'
+  },
+  {
+    title: 'Controller Example',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/src/api/article/controllers/article.ts',
+    localImage: '/assets/projects/controller.avif',
+    id: '11'
+  },
+  {
+    title: 'Model Collection Type',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/src/api/article/content-types/article/schema.json',
+    localImage: '/assets/projects/model-collection-type.avif',
+    id: '12'
+  },
+  {
+    title: 'Simple Service -> Custom Find',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/src/api/article/services/article.ts',
+    localImage: '/assets/projects/simple-service.avif',
+    id: '13'
+  },
+  {
+    title: 'Backend: API-level middleware mutation',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/src/api/article/middlewares/my-middleware.ts',
+    localImage: '/assets/projects/my-middleware.avif',
+    id: '14'
+  },
+  {
+    title: 'Backend: Custom Simple Policy',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/src/api/article/policies/my-policy.ts',
+    localImage: '/assets/projects/custom-policy.avif',
+    id: '15'
+  },
+  {
+    title: 'Webhook',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/frontend/src/app/api/revalidate-path/route.ts',
+    localImage: '/assets/projects/webhook-call.avif',
+    id: '16'
+  },
+  {
+    title: 'CRON Task',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/config/cron-task.ts',
+    localImage: '/assets/projects/cron-task.avif',
+    id: '17'
+  },
+  {
+    title: 'Custom conditions for Role-Based Access Control (RBAC)',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/src/index.ts',
+    localImage: '/assets/projects/custom-conditions.avif',
+    id: '18'
+  },
+  {
+    title: 'Feature Flag Config',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/config/features.ts',
+    localImage: '/assets/projects/feature-flag.avif',
+    id: '19'
+  },
+  {
+    title: 'Custom Widget',
+    web: 'https://github.com/20essentials/some-projects-with-strapi-cms/blob/main/backend/src/plugins-(draft)/content-metrics/admin/src/components/MetricsWidget/index.jsx',
+    localImage: '/assets/projects/custom-widget.avif',
+    id: '20'
+  },
+];
